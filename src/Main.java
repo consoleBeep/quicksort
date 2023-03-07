f1dad7e7ae62da3527a797f0c730c50e97992c41
casper madsen <casp589b@elev.techcollege.dk>
2023-03-07 09:35:48 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		
+		int[] array = {6, 4, 2, 7, 3, 6};
+		
+		quickSort(array, 0, array.length - 1);
+		
+		for (int i : array){
+			
+			System.out.println(i);
+		}
+	}
+	
+	private static void quickSort(int[] array, int low, int high) {
+		
+		if(low <= high) {
+			
+			int pivot = partition(array, low, high);
+			quickSort(array, low, pivot - 1);
+			quickSort(array, pivot + 1, high);
+		}
+		
+	}
+	
+	private static int partition(int[] array, int low, int high) {
+		
+		int pivot = array[high];
+		int i = low - 1;
+		
+		for (int j = low; j <= high -1; j++){
+			
+			if (array[j] < pivot){
+				
+				++i;
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		
+		++i;
+		int temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		return i;
 	}
 }
